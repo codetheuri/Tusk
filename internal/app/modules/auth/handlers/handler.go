5b183cbdc09069b616bc0b7d607bf7bac605baf8
codetheuri <theurij113@gmail.com>
2025-07-25 23:56:31 +0300

    updates

--- internal/app/modules/auth/handlers/handler.go
+++ internal/app/modules/auth/handlers/handler.go
@@ -1,23 +1,38 @@
 package handlers
+
 import (
-   //	"context"
-	//"net/http"
-	authServices "github.com/codetheuri/todolist/internal/app/modules/auth/services"
+	//	"context"
+	"net/http"
+
+	 "github.com/codetheuri/todolist/internal/app/modules/auth/services"
 	"github.com/codetheuri/todolist/pkg/logger"
+	"github.com/codetheuri/todolist/pkg/validator"
+	"github.com/codetheuri/todolist/pkg/validators"
 	//"github.com/codetheuri/todolist/pkg/web"
 	//"github.com/codetheuri/todolist/internal/app/modules/auth/models"
 )
 
-type AuthHandler struct {
-       authService authServices.AuthService
+type AuthHandler interface{
+	Register(w http.ResponseWriter, r *http.Request)
+	Login(w http.ResponseWriter, r *http.Request)
+	GetUserProfile(w http.ResponseWriter, r *http.Request)
+	ChangePassword(w http.ResponseWriter, r *http.Request)
+	DeleteUser(w http.ResponseWriter, r *http.Request)
+	RestoreUser(w http.ResponseWriter, r *http.Request)
+	Logout(w http.ResponseWriter, r *http.Request)
+}
+type authHandler struct {
+       authServices services.AuthService
 	   log logger.Logger
+	   validator *validator.Validator
 }
 
 // constructor for AuthHandler
-func NewAuthHandler(authService authServices.AuthService, log logger.Logger) *AuthHandler {
-	return &AuthHandler{
-		authService: authService,
+func NewAuthHandler(authServices *services.AuthService, log logger.Logger, validator *validators.Validator) AuthHandler {
+	return &authHandler{
+		authServices: authServices,
 		log: log,
+		validator: validator,
 	}
 }
 
