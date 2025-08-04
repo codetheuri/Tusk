2160cf04aa0d24a8624c9e018f79ec437f51fe81
codetheuri <theurij113@gmail.com>
2025-08-04 22:30:45 +0300

    fixed docker issue

--- Dockerfile
+++ Dockerfile
@@ -13,6 +13,8 @@ COPY . .
 # Build the application
 RUN CGO_ENABLED=0 GOOS=linux go build -o tusk cmd/main.go
 
+# RUN CGO_ENABLED=0 GOOS=linux go build -o /usr/local/bin/tusk_migrate ./cmd/migrate
+RUN CGO_ENABLED=0 GOOS=linux go build -o tusk_migrate ./cmd/migrate
 # Runtime stage
 FROM alpine:latest
 
@@ -20,8 +22,9 @@ WORKDIR /root/
 
 # Copy the binary from builder
 COPY --from=builder /app/tusk .
+COPY --from=builder /app/tusk_migrate .
 COPY --from=builder /app/.env .
 
-EXPOSE 8080
+EXPOSE 8081
 
 CMD ["./tusk"]
--- cmd/main.go
+++ cmd/main.go
@@ -4,7 +4,7 @@ import (
 	"github.com/codetheuri/todolist/config"
 	"github.com/codetheuri/todolist/internal/bootstrap"
 	"github.com/codetheuri/todolist/pkg/logger"
-	"github.com/codetheuri/todolist/pkg/mailer"
+	// "github.com/codetheuri/todolist/pkg/mailer"
 )
 
 func main() {
@@ -20,18 +20,18 @@ func main() {
 	}
 
 	//test mail
-	mailerService := mailer.NewMailerService(cfg, log)
-	log.Info("--- Attempting to send test email ---")
-	testRecipient := "proffsyntax5@gmail.com" 
-	testSubject := "Tusk Mailer Test from CLI"
-	testBody := "Hello from Tusk! This is a test email sent using Go's net/smtp. If you see this, the mailer is working!"
-	err = mailerService.SendEmail([]string{testRecipient}, testSubject, testBody)
-    if err != nil {
-		log.Error("Failed to send test email", err)
-	} else {
-		log.Info("Test email sent successfully! Check your inbox for " + testRecipient)
-	}
-	log.Info("--- Test email attempt finished ---")
+	// mailerService := mailer.NewMailerService(cfg, log)
+	// log.Info("--- Attempting to send test email ---")
+	// testRecipient := "theurij113@gmail.com" 
+	// testSubject := "Tusk Mailer Test from CLI"
+	// testBody := "Hello from Tusk! This is a test email sent using Go's net/smtp. If you see this, the mailer is working!"
+	// err = mailerService.SendEmail([]string{testRecipient}, testSubject, testBody)
+    // if err != nil {
+	// 	log.Error("Failed to send test email", err)
+	// } else {
+	// 	log.Info("Test email sent successfully! Check your inbox for " + testRecipient)
+	// }
+	// log.Info("--- Test email attempt finished ---")
 
 
 	log.Info("Configuration loaded successfully")
