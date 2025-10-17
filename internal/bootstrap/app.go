f65f64803d57d694574559ce753a2d9129f75cc3
codetheuri <theurij113@gmail.com>
2025-10-17 22:46:16 +0300

    idiomatic way to handle port assignment

--- internal/bootstrap/app.go
+++ internal/bootstrap/app.go
@@ -2,14 +2,15 @@ package bootstrap
 
 import (
 	"fmt"
+	"net"
 	"net/http"
 
 	"github.com/codetheuri/todolist/config"
 	modules "github.com/codetheuri/todolist/internal/app"
 
-	todoModule "github.com/codetheuri/todolist/internal/app/todo"
-	authModule  "github.com/codetheuri/todolist/internal/app/auth"
+	authModule "github.com/codetheuri/todolist/internal/app/auth"
 	router "github.com/codetheuri/todolist/internal/app/routers"
+	todoModule "github.com/codetheuri/todolist/internal/app/todo"
 	"github.com/codetheuri/todolist/internal/platform/database"
 	"github.com/codetheuri/todolist/pkg/logger"
 	"github.com/codetheuri/todolist/pkg/middleware"
@@ -32,10 +33,10 @@ func Run(cfg *config.Config, log logger.Logger) error {
 
 	//application modules
 	var appModules []modules.Module
-    authMod := authModule.NewModule(db, log, appValidator, cfg)
-	 // Example of adding a new module))
-	appModules = append(appModules, authModule.NewModule( db, log, appValidator,cfg)) // Example of adding a new module
-     appModules = append(appModules, todoModule.NewModule(db, log, appValidator,authMod.TokenService))
+	authMod := authModule.NewModule(db, log, appValidator, cfg)
+	// Example of adding a new module))
+	appModules = append(appModules, authModule.NewModule(db, log, appValidator, cfg)) // Example of adding a new module
+	appModules = append(appModules, todoModule.NewModule(db, log, appValidator, authMod.TokenService))
 	//register routes from all modules
 	mainRouter := router.NewRouter(log)
 	for _, module := range appModules {
@@ -48,11 +49,16 @@ func Run(cfg *config.Config, log logger.Logger) error {
 	handler = middleware.Recovery(log)(handler)
 	handler = middleware.RequestID()(handler)
 
-	//Start Server
-	serverAddr := fmt.Sprintf(":%d", cfg.ServerPort)
-	// serverAddr := ":8080"
-	log.Info(fmt.Sprintf("Server starting on %v", serverAddr))
-	if err := http.ListenAndServe(serverAddr, handler); err != nil {
+	ln, err := net.Listen("tcp", fmt.Sprintf(":%d", cfg.ServerPort))
+	if err != nil {
+		return fmt.Errorf("failed to start listener: %w", err)
+	}
+
+	// get the actual address assigned (useful if port was 0)
+	actualAddr := ln.Addr().(*net.TCPAddr)
+	log.Info(fmt.Sprintf("Server is listening on port %d", actualAddr.Port))
+
+	if err := http.Serve(ln, handler); err != nil {
 		return fmt.Errorf("server failed to start: %w", err)
 	}
 
